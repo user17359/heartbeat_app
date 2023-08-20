ff414e35fe72abb72d7727bf673337b11723efae
Ryszard Błażej <sapermysza@gmail.com>
2023-08-20 23:01:39 +0200

    Initial commit

---
+++ app/src/main/java/agh/ryszard/blazej/heartbeat_app/MainActivity.kt
@@ -0,0 +1,46 @@
+package agh.ryszard.blazej.heartbeat_app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import agh.ryszard.blazej.heartbeat_app.ui.theme.Heartbeat_appTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Heartbeat_appTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Heartbeat_appTheme {
+        Greeting("Android")
+    }
+}
